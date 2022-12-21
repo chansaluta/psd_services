138e3d0b37d0a8d19f465fc68ec801ac8a315d99
Christian M. Saluta <ischaaaaanz@gmail.com>
2022-12-21 09:40:12 +0800

    Initial Commit

---
+++ apps/mta_server/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MtaServerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.mta_server'
---
+++ apps/rrptp_server/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RrptpServerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.rrptp_server'
