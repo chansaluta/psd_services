814aca88ed84454d958e6862b325466e325a95a3
Christian M. Saluta <ischaaaaanz@gmail.com>
2023-01-10 11:28:18 +0800

    SFP Forms

--- apps/sfp_server/urls.py
+++ apps/sfp_server/urls.py
@@ -6,6 +6,19 @@ from apps.sfp_server.views import *
 urlpatterns = [
     path('', index, name='sfp_page' ),
     path('dashboard/', dashboard, name='dashboard' ),
-    path('status_tool/add/', nut_status_add_data, name='nut_status_add_data'),
+    # ! Status Tool Directory Path
+    path('status_tool/', nut_status_add_data, name='nut_status_add_data'),
+
+    # ! Children Status Directory Path
+    path('children_status/underweight/', underweight_children, name='underweight_children' ),
+    path('children_status/stunted/', stunted_children, name='stunted_children' ),
+    path('children_status/moderately_wasted/', moderately_wasted_children, name='moderately_wasted_children' ),
+    path('children_status/severely_underweight/', severely_underweight_children, name='severely_underweight_children' ),
+    path('children_status/severely_stunted/', severely_stunted_children, name='severely_stunted_children'  ),
+    path('children_status/severely_wasted/', severely_wasted_children, name='severely_wasted_children' ),
+    
+    path('summary/', summary, name='summary')
+    
+
 
 ]
--- apps/sfp_server/views.py
+++ apps/sfp_server/views.py
@@ -13,6 +13,50 @@ def dashboard(request):
     return render(request, template)
 
 def nut_status_add_data(request):
-    template = "sfp/pages/status_tool/add_data.html"
+    template = "sfp/pages/status_tool.html"
+
+    return render(request, template)
+
+# ? Children Status List
+
+
+def moderately_wasted_children(request):
+    template = "sfp/pages/status_list/moderately_wasted.html"
+
+    return render(request, template)
+
+
+def severely_stunted_children(request):
+    template = "sfp/pages/status_list/severely_stunted.html"
+
+    return render(request, template)
+
+
+def severely_underweight_children(request):
+    template = "sfp/pages/status_list/severely_underweight.html"
+
+    return render(request, template)
+
+
+def severely_wasted_children(request):
+    template = "sfp/pages/status_list/severely_wasted.html"
+
+    return render(request, template)
+
+
+def stunted_children(request):
+    template = "sfp/pages/status_list/stunted.html"
+
+    return render(request, template)
+
+
+def underweight_children(request):
+    template = "sfp/pages/status_list/underweight.html"
+
+    return render(request, template)
+
+
+def summary(request):
+    template = "sfp/pages/summary.html"
 
     return render(request, template)
